590f1360564535d73ef03a31153411eefece227f
Adesegun Adedeji <adesegunadedeji@gmail.com>
2020-10-10 19:34:05 -0700

    testing heroku

--- server.js
+++ server.js
@@ -1,33 +1,34 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
 import session  from 'express-session';
+import cookieparser from 'cookie-parser';
 import mongoose from 'mongoose';
 import morgan from 'morgan';
 import helmet from 'helmet';
-import rfs from 'rotating-file-stream';
-import path from 'path';
-import {Router as api} from './src/index.js'
+// import rfs from 'rotating-file-stream';
+// import path from 'path';
+import { Router as api} from './src/index.js'
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieparser());
 app.use(cors());
 app.use(morgan('combined'));
 app.use(helmet());
-const SECRET = process.env.SECRET;
+
 //MiddleWare for User Login
 app.use(session({
-  secret: SECRET,
+  secret: process.env.SECRET,
   resave:false,
   saveUninitialized:false,
 }))
+
 //MiddleWare
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true })); //can change to false
-app.use(bodyParser.json());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+
 
 app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -36,20 +37,14 @@ app.all('*', function (req, res, next) {
 });
 
 // create a rotating write stream for logging requests daily
-const __dirname = path.resolve();
-const serverLogStream = rfs.createStream('server.log', {
-    interval: '1d',
-    path: path.join(__dirname, 'logs')
-})
-app.use(morgan("combined", { stream: serverLogStream }));
+// const __dirname = path.resolve();
+// const serverLogStream = rfs.createStream('server.log', {
+//     interval: '1d',
+//     path: path.join(__dirname, 'logs')
+// })
+// app.use(morgan("combined", { stream: serverLogStream }));
 const PORT = process.env.PORT;
-//CORS allows request to come in from React
-// const corsOptions={
-//     credentials:true,
-//     origin:'http://localhost:3000',// Include Allowable Origin
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-// app.use(cors(corsOptions));
+
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
@@ -67,14 +62,6 @@ app.use((req,res,next)=>{
     next();
 })
 
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client/build'));
-}
-
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
-
 //server works --200 status
 app.get('/', (req, res) => {
   res.status(200).send('<p style="text-align: center; font-weight: 600">Welcome to VICINAGE API...</p>');
@@ -90,3 +77,11 @@ app.listen(PORT, ()=>{
     console.log(`listening on  PORT ${PORT}`);
 })
 export { app as server };
+
+//CORS allows request to come in from React
+// const corsOptions={
+//     credentials:true,
+//     origin:'http://localhost:3000',// Include Allowable Origin
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
